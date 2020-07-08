247bcdddc77792db4da2e482dd7396b9ad63b245
Jianzhuo <shenjianzhuo@gmail.com>
2020-07-08 15:57:25 +1200

    setup the project development environment, start the project from Home.vue

---
+++ src/App.vue
@@ -0,0 +1,214 @@
+<template>
+  <div id="app">
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+      integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+      crossorigin="anonymous"
+    />
+    <!-- <img src="./assets/logo.png">-->
+
+    <el-container>
+      <!-- 
+      Header part of all webpages for this project
+      with logo and menu 
+      -->
+      <el-header
+        id="pageHeader"
+        height="auto"
+        style="padding:0;"
+      >
+        <el-row style="padding-top: 1em">
+          <el-col :span="4" :xs="2">
+            <div class="grid-content"></div>
+          </el-col>
+          <el-col :span="16" :xs="20">
+            <div class="grid-content">
+              <!-- Logo -->
+              <el-image
+                style="width: 10em; padding: 10px; height: auto;"
+                :src="require('./assets/images/hero_logo.png')"
+                fit="conatin"
+              ></el-image>
+              <!-- Main menu -->
+              <!-- menu button -->
+              <i @click="menuIconclick = !menuIconclick" class="fa fa-list menuIcon"></i>
+            </div>
+          </el-col>
+          <el-col :span="4" :xs="2">
+            <div class="grid-content"></div>
+          </el-col>
+        </el-row>
+
+        <!-- menu content -->
+        <transition name="el-zoom-in-top">
+          <div
+            v-show="menuIconclick"
+            style="position: absolute; padding-top: 90.7px; top: 0; width: 100%; background-color: #b08008; z-index: -1"
+          >
+            <el-menu background-color="#b08008" text-color="#fff">
+              <el-menu-item index="1">
+                <span slot="title">HOME</span>
+              </el-menu-item>
+              <el-menu-item index="2">
+                <span slot="title">ABOUT US</span>
+              </el-menu-item>
+              <el-menu-item index="3">
+                <span slot="title">OUR SERVICES</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <span slot="title">PROJECTS</span>
+              </el-menu-item>
+              <el-menu-item index="5">
+                <span slot="title">AWARDS</span>
+              </el-menu-item>
+              <el-menu-item index="6">
+                <span slot="title">OUR PARTNERS</span>
+              </el-menu-item>
+              <el-menu-item index="7">
+                <span slot="title">CONTACT US</span>
+              </el-menu-item>
+            </el-menu>
+          </div>
+        </transition>
+      </el-header>
+
+      <router-view/>
+
+      
+
+      <el-footer style="padding: 0;">
+        <!-- the top parrt of the footer  -->
+        <div class="footerTop"></div>
+        <!-- the main parrt of the footer -->
+        <div style="background: #b08008; padding-top: 30px">
+          <el-row>
+            <el-col :span="3">
+              <div class="grid-content"></div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content">
+                <i class="el-icon-location footerIcon" aria-hidden="true"></i>
+                <span>PO BOX 84433, Westgate,Auckland,0657</span>
+              </div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content">
+                <i class="fa fa-phone footerIcon" aria-hidden="true"></i>
+                <span>+64 0800 166 899</span>
+              </div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content">
+                <i class="fa fa-envelope footerIcon" aria-hidden="true"></i>
+                <span>info@herointernational.co.nz</span>
+              </div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content"></div>
+            </el-col>
+          </el-row>
+
+          <el-row>
+            <el-col :span="3">
+              <div class="grid-content"></div>
+            </el-col>
+            <el-col :span="18">
+              <div class="grid-content">
+                <el-divider></el-divider>
+                <span style>Copyright © 2020 Hero International. All rights reserved</span>
+              </div>
+            </el-col>
+            <el-col :span="3">
+              <div class="grid-content"></div>
+            </el-col>
+          </el-row>
+        </div>
+      </el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    menuIconclick: false
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      window.onscroll = function() {
+        var headerMain = document.getElementById("pageHeader");
+        if (window.pageYOffset >= 70) {
+          headerMain.classList.add("headerMain-bg");
+        } else {
+          headerMain.classList.remove("headerMain-bg");
+        }
+      };
+    });
+  }
+};
+</script>
+
+<style>
+.headerMain-bg {
+  background: #fff;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menuIcon {
+  font-size: 1.7em;
+  color: #851719;
+  padding-top: 1em;
+  float: right;
+}
+
+.grid-content {
+  min-height: 36px;
+}
+
+header {
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+  z-index: 1;
+}
+
+footer {
+  color: white;
+  font-size: 0.7em; 
+  position: relative; 
+  bottom: 12em;
+}
+
+.footerTop {
+  -webkit-clip-path: polygon(0 0, 50% 50%, 100% 0, 100% 100%, 0 100%);
+  clip-path: polygon(0 0, 50% 99%, 100% 0, 100% 100%, 0 100%);
+  width: 100%;
+  height: 100px;
+  background: #b08008;
+}
+
+.footerIcon {
+  color: white;
+  border: white solid 1px;
+  padding: 4px;
+  border-radius: 50%;
+}
+
+.el-divider--horizontal {
+  margin: 0;
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,223 @@
+<!-- the content of the home page -->
+<template>
+  <div>
+    <!-- For home page move the main content up 90.7px to the header -->
+    <el-main style="padding: 0; position: relative; top: -90.7px">
+      <!-- banner -->
+      <div id="homeBanner">
+        <!-- content box in banner -->
+        <div id="homeBannercontent">
+          <!-- text -->
+          <p
+            style="padding: 1em;"
+          >Delivering Modern Comfort And Enjoyment To Residential And Commercial Spaces</p>
+          <!-- button -->
+          <span
+            style="background-color: #851719; padding: .8em; border-radius: 10px 10px 0 10px;"
+          >Get A Quote</span>
+        </div>
+        <!-- button to scroll page to setted anchor -->
+        <i class="el-icon-arrow-down angleDownicon" aria-hidden="true"></i>
+      </div>
+
+      <!-- why choose us section -->
+      <div id="whyChooseus">
+        <!-- title -->
+        <div style="padding-top: 3em; font-size: 1.5em; font-weight: 600">
+          <span style="color: #851719">WHY</span>
+          <span style="color: #b08008">CHOOSE US</span>
+        </div>
+        <!-- content -->
+        <el-row>
+          <el-col :span="8">
+            <div class="grid-content wContent">
+              <i class="fa fa-star" aria-hidden="true"></i>
+              <span>YEARS EXPERIENCE</span>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam.</p>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="grid-content wContent">
+              <i class="fa fa-list-alt" aria-hidden="true"></i>
+              <span>BEST QUALITY</span>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam.</p>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="grid-content wContent">
+              <i class="fa fa-wrench" aria-hidden="true"></i>
+              <span>THE SOLUTION</span>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam.</p>
+            </div>
+          </el-col>
+        </el-row>
+        <!-- button -->
+        <span
+          style="background-color: #851719; padding: .8em; border-radius: 10px 10px 0 10px; color: white; font-size: .8em;"
+        >More Details</span>
+      </div>
+
+      <!-- Our services part -->
+      <div id="ourServices">
+        <!-- title -->
+        <div style="padding-top: 2em; font-size: 1.5em; font-weight: 600">
+          <span style="color: white">OUR</span>
+          <span style="color: #b08008">SERVICES</span>
+        </div>
+        <!-- description -->
+        <p
+          style="margin-top: 2em; margin-bottom: 2em;"
+        >Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>
+        <!-- content -->
+        <el-row>
+          <el-col :span="8">
+            <div class="grid-content oContent">
+              <el-avatar
+                class="oImage"
+                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+              ></el-avatar>
+              <span>House Design</span>
+              <p>Make your dream house to real</p>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="grid-content oContent">
+              <el-avatar
+                class="oImage"
+                size="large"
+                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+              ></el-avatar>
+              <span>Build / Rebuild</span>
+              <p>Professional team, Professional service</p>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="grid-content oContent">
+              <el-avatar
+                class="oImage"
+                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+              ></el-avatar>
+              <span>Interior Renovation</span>
+              <p>Provide you with the most suitable decoration plan</p>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+
+      <!-- Our partners part -->
+      <div id="ourPartners">
+        <!-- title -->
+        <div style="padding-top: 3em; font-size: 1.5em; font-weight: 600">
+          <span style="color: white">OUR</span>
+          <span style="color: #b08008">PARTNERS</span>
+        </div>
+        <!-- partners logo board -->
+        <el-image style="width: 80%; height: auto; padding-top: 3em;" :src="require('../assets/images/home-partner-logo.png')" :fit="fit"></el-image>
+      </div>
+    </el-main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#homeBanner {
+  background-image: url("../assets/images/home-banner.jpg");
+  background-size: cover;
+  background-position: center;
+  padding-top: 16em;
+  padding-bottom: 8em;
+}
+
+#homeBannercontent {
+  width: 35%;
+  margin: 0 auto;
+  color: white;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 25px;
+  padding-bottom: 2em;
+}
+
+.angleDownicon {
+  color: white;
+  font-size: 1.5em;
+  border-radius: 50%;
+  padding: 3px;
+  border: white solid 1px;
+  margin-top: 4.5em;
+}
+
+#whyChooseus {
+  background-image: url("../assets/images/home-about-bg.jpg");
+  background-size: cover;
+  background-position: center;
+  padding-left: 5%;
+  padding-right: 5%;
+  padding-bottom: 2.3em;
+}
+
+.wContent {
+  border: #851719 solid 1px;
+  text-align: start;
+  padding: 2em;
+  margin: 2em;
+}
+
+.wContent i {
+  font-size: 1.5em;
+  color: #b08008;
+}
+
+.wContent span {
+  color: #851719;
+  font-weight: 700;
+  font-size: 0.9em;
+}
+
+.wContent p {
+  margin-left: 2.5em;
+  font-size: 0.8em;
+}
+
+#ourServices {
+  background-color: #851719;
+  color: white;
+  padding-bottom: 3em;
+}
+
+#ourServices p {
+  width: 45%;
+  margin: 0 auto;
+}
+
+.oImage {
+  display: block;
+  margin: 0 auto;
+  width: 5em;
+  height: 5em;
+  margin-bottom: 1.3em;
+}
+
+.oContent span {
+  font-size: 0.9em;
+  font-weight: 700;
+}
+
+.oContent p {
+  font-size: 0.8em;
+  padding-top: 1em;
+}
+
+#ourPartners {
+  background-image: url("../assets/images/home-partner-bg.jpg");
+  padding-bottom: 10em;
+}
+</style>
